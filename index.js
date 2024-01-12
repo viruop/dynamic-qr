27635bffd05d63c3fa5cca8702c445f55fba0733
viruop <virajrai706@gmail.com>
2024-01-12 13:50:23 +0530

    few optimizations

---
+++ index.js
@@ -0,0 +1,72 @@
+import { createCanvas, loadImage } from "canvas";
+import { readdir, existsSync, mkdirSync } from "fs";
+import { jsPDF } from "jspdf";
+
+const folderPath = "./qrcodes";
+const dir = "./output";
+
+readdir(folderPath, async (err, files) => {
+  const startTime = performance.now();
+
+  if (err) {
+    console.error("Error reading folder:", err);
+    return;
+  }
+  const bg = await loadImage("images/bg.jpeg");
+  await Promise.allSettled(
+    files.map(async (file) => {
+      if (file.includes(".DS_Store")) return;
+      await createImage(file, bg).catch(console.error);
+    })
+  );
+  console.log("Folder created successfully");
+  const endTime = performance.now();
+  const timeTaken = endTime - startTime;
+  const minutes = Math.floor((timeTaken / 1000 / 60) << 0);
+  const sec = Math.floor((timeTaken / 1000) % 60);
+
+  console.log(`Time taken: ${minutes}:${sec} minutes`);
+});
+
+async function createImage(text, bg) {
+  // Create canvas with the same dimensions as the background image
+  const canvas = createCanvas(bg.width, bg.height);
+  const ctx = canvas.getContext("2d");
+  // Draw background image on canvas
+  ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
+  // Generate QR code based on input text
+  const qrCodeImage = await loadImage(`./qrcodes/${text}`);
+  // Draw QR code image at the center
+  ctx.drawImage(
+    qrCodeImage,
+    220,
+    450,
+    qrCodeImage.width / 2,
+    qrCodeImage.height / 2
+  );
+  // Add text below the QR code
+  const fontSize = 36;
+  const textX = canvas.width / 2;
+  const textY = -150 + qrCodeImage.height + fontSize + 10; // Add some padding below the QR code
+  ctx.font = `${fontSize}px Arial`;
+  ctx.textAlign = "center";
+  ctx.fillStyle = "black";
+  const upiId = text?.split(".png")[0];
+  ctx.fillText(upiId, textX, textY);
+
+  const containerWidth = 4.134;
+  const containerHeight = 6.024;
+
+  var doc = new jsPDF({
+    orientation: "p",
+    unit: "in",
+    format: [containerWidth, containerHeight],
+  });
+  var width = doc.internal.pageSize.getWidth();
+  var height = doc.internal.pageSize.getHeight();
+  doc.addImage(canvas.toDataURL(), "JPEG", 0, 0, width, height, "", "FAST");
+  if (!existsSync(dir)) {
+    mkdirSync(dir);
+  }
+  doc.save(`${dir}/${upiId}.pdf`);
+}
